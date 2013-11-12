6577ac1f6b08b722ed3a6d3a1f0310c03fbc67a2
tech-Yu-Mac <yujie@tech-yu-mac.lan>
2013-11-12 11:06:26 +0800

    Initial Commit

---
+++ MyDemo2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyDemo2
+//
+//  Created by tech-Yu-Mac on 13-11-12.
+//  Copyright (c) 2013年 yujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
